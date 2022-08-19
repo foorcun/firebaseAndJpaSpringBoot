0346c1bc085e2fe761e525a82661da0663d8baca
msnifoorcun@hotmail.com <Muse2321>
2022-08-19 13:44:14 +0300

    flutter read ve create eklendi

--- src/main/java/com/javacourse/project/hibernateAndJpa_v3/restApi/CRUDController.java
+++ src/main/java/com/javacourse/project/hibernateAndJpa_v3/restApi/CRUDController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
+@CrossOrigin
 @RestController
 public class CRUDController {
     public CRUDService crudService;
@@ -31,7 +32,7 @@ public class CRUDController {
     }
 
     @PutMapping("/delete")
-    public String deleteCRUD(@RequestParam String documentId){
+        public String deleteCRUD(@RequestParam String documentId){
         return crudService.deleteCRUD(documentId);
     }
     @GetMapping("/test")
